5ea3bd22aeb2f480ade910f70478b71e19c58af5
wancat <wancat@pm.me>
2021-03-31 09:02:03 +0800

    Refactor with class

--- sampling.py
+++ sampling.py
@@ -2,35 +2,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 time_of_view = 0.5
-analog_time = np.linspace(0, time_of_view, 10**6)
 
-sampling_rate = 40
-sampling_period = 1. / sampling_rate
-sample_number = time_of_view / sampling_period
-sampling_time = np.linspace(0, time_of_view, int(sample_number))
 
-carrier_frequency = 20
-amplitude = 1
-phase = 0
+class Sampler:
+    def __init__(self, frequency=10, phase=0, sampling_rate=20, quantizing_bits=4):
+        self.frequency = frequency
+        self.phase = 0
+        self.analog_time = np.linspace(0, time_of_view, 10**6)
+        sample_number = time_of_view * sampling_rate
+        self.sampling_time = np.linspace(0, time_of_view, int(sample_number))
+        quantizing_levels = 1 << quantizing_bits
+        self.quantizing_step = 2 / quantizing_levels
 
-quantizing_bits = 16
-quantizing_levels = 2 ** quantizing_bits
-quantizing_step = 2 * amplitude / quantizing_levels
+    def signal(self, t):
+        return np.cos(2 * np.pi * self.frequency * t + self.phase)
 
-def analog_signal(time_point):
-    return amplitude * np.cos(2 * np.pi * carrier_frequency * time_point + phase)
+    def analog_signal(self):
+        return self.signal(self.analog_time)
 
-sampling_signal = analog_signal(sampling_time)
-quantizing_signal = np.round(sampling_signal / quantizing_step) * quantizing_step
+    def sampling_signal(self):
+        return self.signal(self.sampling_time)
 
-plt.plot(analog_time, analog_signal(analog_time))
-plt.stem(sampling_time, quantizing_signal, linefmt='r-', markerfmt='rs', basefmt='r-')
-plt.title("Analog to digital signal conversion")
-plt.xlabel("Time")
-plt.ylabel("Amplitude")
-plt.show()
+    def quantizing_signal(self):
+        return np.round(self.sampling_signal() / self.quantizing_step) * self.quantizing_step
 
+
+if __name__ == '__main__':
+    sampler = Sampler()
+    plt.plot(sampler.analog_time, sampler.analog_signal())
+    #plt.stem(sampling_time, quantizing_signal, linefmt='r-', markerfmt='rs', basefmt='r-')
+    plt.plot(sampler.sampling_time, sampler.quantizing_signal())
+    plt.title("Analog to digital signal conversion")
+    plt.xlabel("Time")
+    plt.ylabel("Amplitude")
+    plt.show()
+
+"""
 plt.stem(sampling_time, sampling_signal - quantizing_signal, linefmt='r-', markerfmt='rs', basefmt='r-')
 plt.xlabel("Time")
 plt.ylabel("Quantization error")
 plt.show()
+"""
